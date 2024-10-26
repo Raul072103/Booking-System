f730748bc6cc5257f6e6b073bc9e8a263e6f0e35
raul <raulandrei2017@yahoo.com>
2024-10-26 17:29:02 +0300

    Add testing

---
+++ internal/render/render_test.go
@@ -0,0 +1,78 @@
+package render
+
+import (
+	"github.com/raul/BookingSystem/internal/models"
+	"net/http"
+	"testing"
+)
+
+func TestCreateTemplateCache(t *testing.T) {
+	pathToTemplates := "./../../templates"
+	_, err := CreateTemplateCache(pathToTemplates)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestNewTemplates(t *testing.T) {
+	NewTemplates(app)
+}
+
+func TestRenderTemplate(t *testing.T) {
+	pathToTemplates := "./../../templates"
+	tc, err := CreateTemplateCache(pathToTemplates)
+	if err != nil {
+		t.Error(err)
+	}
+
+	app.TemplateCache = tc
+	r, err := getSession()
+	if err != nil {
+		t.Error(err)
+	}
+
+	ww := myWriter{}
+
+	err = Template(&ww, r, "home.page.gohtml", &models.TemplateData{})
+	if err != nil {
+		t.Error("rendered template that doesn't exist")
+	}
+}
+
+func TestAddDefaultData(t *testing.T) {
+	var td models.TemplateData
+	r, err := getSession()
+	if err != nil {
+		t.Error(err)
+	}
+
+	session.Put(r.Context(), "flash", "flash_value")
+	session.Put(r.Context(), "warning", "warning_value")
+	session.Put(r.Context(), "error", "error_value")
+	result := AddDefaultData(&td, r)
+
+	if result.Flash != "flash_value" {
+		t.Error("Flash value different!")
+	}
+
+	if result.Warning != "warning_value" {
+		t.Error("Warning value different!")
+	}
+
+	if result.Error != "error_value" {
+		t.Error("Error value different!")
+	}
+}
+
+func getSession() (*http.Request, error) {
+	r, err := http.NewRequest("GET", "/some-url", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X-Session"))
+	r = r.WithContext(ctx)
+
+	return r, nil
+}
---
+++ internal/render/setup_test.go
@@ -0,0 +1,51 @@
+package render
+
+import (
+	"encoding/gob"
+	"github.com/alexedwards/scs/v2"
+	"github.com/raul/BookingSystem/internal/config"
+	"github.com/raul/BookingSystem/internal/models"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = testApp.InProduction
+
+	testApp.Session = session
+
+	app = &testApp
+	os.Exit(m.Run())
+}
+
+type myWriter struct{}
+
+func (tw *myWriter) Header() http.Header {
+	var h http.Header
+	return h
+}
+
+func (tw *myWriter) Write(b []byte) (int, error) {
+	length := len(b)
+	return length, nil
+}
+
+func (tw *myWriter) WriteHeader(statusCode int) {
+
+}
