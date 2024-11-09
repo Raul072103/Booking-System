eb97258e2e50565cd695f53471f4288c6b76c5b0
raul <raulandrei2017@yahoo.com>
2024-11-09 14:15:00 +0200

    new small changes

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,12 +9,8 @@ type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
-
 	InsertRoomRestriction(r models.RoomRestriction) error
-
 	SearchAvailabilityByDateByRoomId(start, end time.Time, roomId int) (bool, error)
-
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
-
 	GetRoomById(id int) (models.Room, error)
 }
---
+++ migrations/20241109120654_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restrictions_name,created_at,updated_at) VALUES
+('reservation','2024-11-02 00:00:00.000','2024-11-04 00:00:00.000'),
+('owner blocked','2024-11-10 00:00:00.000','2024-11-20 00:00:00.000');
